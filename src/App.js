d177b2175335f4b922769049b8a4e56bfabed493
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-09-16 19:44:40 -0500

    start a new project

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import MyFirstComponent from "./components/My-First-Component";
-import TableClassComponent from "./components/Table-Class-Component";
-import TableSimpleComponent from "./components/Table-Simple-Component";
+import React, {Component} from "react";
+import Search from "./components/Search";
 
-function App() {
-    let name = 'Carlos';
-    let greeting = <h2>Hello, my name is {name}</h2>
+class App extends Component {
+    searchData = (term) => {
+        console.log(term);
+    }
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-
-                <p>This is my first React App</p>
-                <h6> {'Hello World from React'}</h6>
-                {greeting}
-
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-            <section className="container">
-                <MyFirstComponent/>
-                <TableClassComponent/>
-            </section>
-            <br/>
-            <div>
-                <TableSimpleComponent/>
+    render() {
+        return (
+            <div className='app container'>
+                <div className='jumbotron'>
+                    <p className='lead text-center'>Search Image</p>
+                    <Search
+                        searchData={this.searchData}/>
+                </div>
             </div>
-        </div>
-    );
+        )
+    }
 }
 
 export default App;
